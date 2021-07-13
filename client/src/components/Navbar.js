36d4360986831b6fa1c6fc4f0a1145c3d554ee0f
Norman Cerros <n_cerros@hotmail.com>
2021-07-12 20:01:35 -0600

    added two files

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+ /*eslint-disable */
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+            <nav>
+    <div class="nav-wrapper #81c784 green lighten-2">
+      <a href="#" class="brand-logo left">GOODFOOD</a>
+      <ul id="nav-mobile" class="right">
+        <li><a href="#" className="waves-effect waves-light btn #66bb6a green lighten-1">LOGIN</a></li>
+        <li><a href="#" className="waves-effect waves-light btn #66bb6a green lighten-1">SIGNUP</a></li>
+      </ul>
+    </div>
+  </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
