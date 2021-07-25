a495fb84957554c439e7cde03394f4ae019c3070
Javan <71285290+djavanw@users.noreply.github.com>
2021-07-25 17:40:37 -0600

    A fixed the nav bar to make it responsive to size change.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,14 @@
 import React, { useEffect } from 'react';
 import API from "../../utils/API";
+import M from "materialize-css";
 
 function Nav () {
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
   
   useEffect(() => {
     API.getCurrentUser().then(results => {
@@ -20,15 +27,15 @@ function Nav () {
       <div className="#a5d6a7 green lighten-3">
         <nav className="#1b5e20 green darken-4">
           <div className="nav-wrapper #1b5e20 green darken-4 container">
-            <a href="/" className="brand-logo left">Good Foods</a>
+            <a href="/" className="brand-logo right">Good Foods</a>
             <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <ul id="nav-mobile" className="left hide-on-med-and-down container">
               {!localStorage.getItem("token") ? 
                 <>
 
-                  <li><a href="/about" className="waves-effect waves-light btn #66bb6a green lighten-1">ABOUT US</a></li>
                   <li><a href="/login" className="waves-effect waves-light btn #66bb6a green lighten-1">LOGIN</a></li>
-                  <li><a href="/register" className="waves-effect waves-light btn #66bb6a green lighten-1">REGISTER</a></li> 
+                  <li><a href="/register" className="waves-effect waves-light btn #66bb6a green lighten-1">REGISTER</a></li>
+                  <li><a href="/about" className="waves-effect waves-light btn #66bb6a green lighten-1">ABOUT US</a></li>
                 </> 
               : 
                 <>
@@ -42,6 +49,25 @@ function Nav () {
               }
                          
             </ul>
+            {!localStorage.getItem("token") ? 
+            <>
+            <ul class="sidenav" id="mobile-demo">
+              <li><a href="/login" className="waves-effect waves-light btn #66bb6a green lighten-1">LOGIN</a></li>
+              <li><a href="/register" className="waves-effect waves-light btn #66bb6a green lighten-1">REGISTER</a></li>
+              <li><a href="/about" className="waves-effect waves-light btn #66bb6a green lighten-1">ABOUT US</a></li>
+            </ul>
+            </>
+            :
+            <>
+            <ul class="sidenav" id="mobile-demo">
+              <li><a href="/generate" className="waves-effect waves-light btn #66bb6a green lighten-1">Meal Plan</a></li>
+              <li><a href="/grocery" className="waves-effect waves-light btn #66bb6a green lighten-1">Make Grocery List</a></li>
+              <li><a href="/savedgrocery" className="waves-effect waves-light btn #66bb6a green lighten-1">View Grocery List</a></li>
+              <li><a href="/product" className="waves-effect waves-light btn #66bb6a green lighten-1">PRODUCT SEARCH</a></li>
+              <li><a href="/login" className="waves-effect waves-light btn #66bb6a green lighten-1" onClick={logout}>LOG OUT</a></li>
+            </ul>
+            </>
+            }
           </div>
             
         </nav>
@@ -52,27 +78,3 @@ function Nav () {
 
 
 export default Nav;
-
-
-// /*eslint-disable */
-// class Nav extends Component {
-//   render() {
-//       return (
-//         <div>
-//           <nav>
-//             <div className="nav-wrapper #81c784 green lighten-2">
-//               <a href="#" className="brand-logo left">Good Foods</a>
-//               <ul id="nav-mobile" className="right">
-//                 <li><a href="/" className="waves-effect waves-light btn #66bb6a green lighten-1">HOME</a></li>
-//                 <li><a href="/product" className="waves-effect waves-light btn #66bb6a green lighten-1">PRODUCT SEARCH</a></li>
-//                 <li><a href="/login" className="waves-effect waves-light btn #66bb6a green lighten-1">LOGIN</a></li>
-//                 <li><a href="/register" className="waves-effect waves-light btn #66bb6a green lighten-1">REGISTER</a></li>
-//               </ul>
-//             </div>
-//           </nav>
-//         </div>
-//       )
-//   }
-// }
-
-// export default Nav;
